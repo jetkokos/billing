b2b39909e73c0c2240d0d5120f29fec75e031f3a
jetKoKoS <afeniks@bk.ru>
2018-06-20 14:30:43 +0300

    smoothing

--- js/script.js
+++ js/script.js
@@ -1,15 +1,42 @@
-function showHidePopup() {
-	document.querySelector("#popup").classList.toggle('invisible');
-}
-
-function showHidePopupSuccess() {
-	document.querySelector("#popup-success").classList.toggle('invisible');
-}
+/*Плавное появление Popup*/
+function showPopup() {
+	document.querySelector(".popup").classList.add('visible');
+};
 
 function hidePopup() {
-	document.querySelector("#popup").classList.add('invisible');
-}
+	document.querySelector(".popup").classList.remove('visible');
+};
+
+function showPopupSuccess() {
+	document.querySelector(".popup-success").classList.add('visible');
+};
 
 function hidePopupSuccess() {
-	document.querySelector("#popup-success").classList.add('invisible');
+	document.querySelector(".popup-success").classList.remove('visible');
+};
+
+/*Плавный переход к якорям*/
+
+var linkNav = document.querySelectorAll('[href^="#nav"]'),
+    V = 0.2;  // скорость, может иметь дробное значение через точку
+for (var i = 0; i < linkNav.length; i++) {
+  linkNav[i].addEventListener('click', function(e) {
+    e.preventDefault();
+    var w = window.pageYOffset,  // прокрутка
+        hash = this.href.replace(/[^#]*(.*)/, '$1');  // id элемента, к которому нужно перейти
+        t = document.querySelector(hash).getBoundingClientRect().top,  // отступ от окна браузера до id
+        start = null;
+    requestAnimationFrame(step); 
+    function step(time) {
+      if (start === null) start = time;
+      var progress = time - start,
+          r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
+      window.scrollTo(0,r);
+      if (r != w + t) {
+        requestAnimationFrame(step)
+      } else {
+        location.hash = hash  // URL с хэшем
+      }
+    }
+  }, false);
 }
